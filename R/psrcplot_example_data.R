e8dd876d7f5ec1b9d2d1571063b33c1e67515c46
jensenmj <mjensen@psrc.org>
2023-04-05 12:07:37 -0700

    Fix documentation per checks.

--- R/psrcplot_example_data.R
+++ R/psrcplot_example_data.R
@@ -11,3 +11,13 @@
 #' see frequently-asked-questions.Rmd vignette for script
 #'
 "summary_pums_example_data"
+
+#' Airport example data included in psrcplot
+#'
+#' SELECT ad.loc_id, ad.airport_name, ef.data_year, ef.enplanements
+#' FROM Elmer.faa.airport_dims AS ad JOIN Elmer.faa.enplanement_facts AS ef 
+#' ON ad.airport_dim_id=ef.airport_dim_id
+#' WHERE ad.st='WA' AND ad.airport_dim_id IN(657,671)
+#' ORDER BY ad.airport_name, ef.data_year;
+#'
+"enplanement_example_data"
